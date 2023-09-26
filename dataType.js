8ad5afae0e076e9002a705f3e434a85706387f1a
Mohamed Ibrahim <crowwnzz3@gmail.com>
2023-09-26 18:28:54 +0530

    second commit

---
+++ dataType.js
@@ -0,0 +1,8 @@
+"use strict";
+let lName = 'Mohamed';
+let dob = "25";
+let result = parseInt(dob);
+let isValid;
+let empList = ["st1"];
+let c = 2 /* Color.Blue */;
+console.log(c);
---
+++ dataType.ts
@@ -0,0 +1,25 @@
+let lName = 'Mohamed';
+
+let dob = "25";
+
+let result = parseInt(dob);
+
+let isValid:boolean;
+
+let empList = ["st1" ]
+
+const enum Color {
+    Red,
+    Green,
+    Blue
+}
+
+
+let c :Color = Color.Blue
+
+let department
+department = 'it';
+department = 32;
+
+
+console.log(c)
